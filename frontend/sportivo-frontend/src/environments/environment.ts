4a2c2f47a8c80a863c707240313cbf28acd4c416
WW <ww@wwology.tech>
2023-01-07 04:11:53 +0000

    initial setup

---
+++ frontend/sportivo-frontend/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'sportivopos',
+    appId: '1:750793514124:web:a5b7ec353cbdc3f666efa9',
+    storageBucket: 'sportivopos.appspot.com',
+    locationId: 'asia-east2',
+    apiKey: 'AIzaSyDjcMqmBr3YxuE1Q-A6fGIm9G5gudP-78Y',
+    authDomain: 'sportivopos.firebaseapp.com',
+    messagingSenderId: '750793514124',
+    measurementId: 'G-PDYKYM5VS5',
+  },
+  production: false,
+//
